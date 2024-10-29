8cf42dac646ccc4d89c804489dc880e045a9aadf
George Davis <george.davis@ibexlabs.com>
2024-10-29 19:13:18 -0400

    feat: Adding a new Lambda function to handle Archera API calls internally within our AWS account rather than the CloudFormation template in customer's AWS environment for better security and safety

---
+++ http-api-lambda-archera-api-onboarding/archera/archera.py
@@ -0,0 +1,173 @@
+import logging
+import urllib3
+import json
+from os import environ
+
+from utils.utils import Utils
+from s3.s3 import s3CopyFiles
+
+class Archera:
+
+    # Archera Constructor
+    # logger: Logger object
+    #
+    # Returns: Archera object
+    # Raises: None
+    def __init__(self, logger: logging.Logger, base_url: str, region_name: str):
+
+        self.logger = logger
+        self.utils = Utils(logger=self.logger)
+        self.http = urllib3.PoolManager()
+        self.base_url = base_url
+        self.region_name = region_name
+
+        ssm_key_partner_org_account_id = environ.get('SSM_KEY_ARCHERA_PARTNER_ORG_ID') # Archera Partner Account ID
+        ssm_key_partner_api_key = environ.get('SSM_KEY_ARCHERA_PARTNER_API_KEY')
+
+        self.partner_account_id = self.utils.get_aws_secret(
+            secret_arn=ssm_key_partner_org_account_id,
+            region_name=region_name
+        )        
+        self.partner_api_key = self.utils.get_aws_secret(
+            secret_arn=ssm_key_partner_api_key,
+            region_name=self.region_name
+        )
+
+    # __get_base_url: Returns str with the customized URL for the Archera REST APIs which includes the base URL and Account ID parameter.
+    def __get_base_url(self, account_id: str) -> str:
+        return self.base_url + '/org/' + account_id
+
+    # __get_headers: Returns the headers for the Archera REST APIs. Returns dict with the headers.
+    def __get_headers(self, api_key: str) -> dict:
+
+        import base64
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + base64.b64encode(bytes(api_key, 'utf-8')).decode('utf-8')
+        }
+    
+    # __create_child_account: Creates a new Account under the Archera Partner account using REST APIs. Returns str with the new Account ID.
+    def __create_child_account(self, httpHeaders: dict, partner_account_id: str, child_account_name: str) -> str:
+        '''
+            The HTTP Body can contain the following information
+                - company (string, required): Name of the child account
+                - labra_subscription_id (string, optional): Labra subscription ID
+                - email (string, optional): Email associated with the child account
+        '''
+        httpBody = {
+            'company': child_account_name
+        }
+        try:
+            self.logger.debug('Archera URL: ' + self.__get_base_url(account_id=partner_account_id) + '/partners/onboarding/register_child')
+            r = self.http.request(
+                'POST', self.__get_base_url(account_id=partner_account_id) + '/partners/onboarding/register_child',
+                headers=httpHeaders,
+                body=json.dumps(httpBody)
+            )
+            response = json.loads(r.data)
+            self.logger.debug('Create Child Account Response: ' + str(response))
+            if r.status == 200:
+                return response['org_id']
+            else:
+                self.logger.exception(response)
+                if len(response.keys()) == 1:
+                    raise Exception('Unable to initiate Archera Onboarding for child Account ' + str(list(response.keys())[0]).capitalize() + ': ' + str(response[list(response.keys())[0]]).capitalize() + '.')
+                elif 'code' in response.keys():
+                    raise Exception('Unable to create new Archera Account Error ' + str(response['code']) + ': ' + response['status'] + '.')
+                else:
+                    raise Exception('Unable to create new Archera Account Error: ' + str(response) + '.')
+        except Exception:
+            raise
+
+    # __init_child_account_onboarding: Creates a new Account under the Archera Partner account using REST APIs. Returns str with the new Account ID.
+    def __init_child_account_onboarding(self, httpHeaders: dict, child_account_id: str) -> str:
+        try:
+            r = self.http.request(
+                'POST', self.__get_base_url(account_id=child_account_id) + '/partners/onboarding/start',
+                headers=httpHeaders
+            )
+            response = json.loads(r.data)
+            self.logger.debug('Init Child Account Onboarding Response: ' + str(response))
+            if r.status == 200:
+                return response['onboarding_id']
+            else:
+                self.logger.exception(response)
+                if len(response.keys()) == 1:
+                    raise Exception('Unable to initiate Archera Onboarding for child Account ' + str(list(response.keys())[0]).capitalize() + ': ' + str(response[list(response.keys())[0]]).capitalize() + '.')
+                elif 'code' in response.keys():
+                    raise Exception('Unable to initiate Archera Onboarding for child Account Error ' + str(response['code']) + ': ' + response['status'] + '.')
+                else:
+                    raise Exception('Unable to initiate Archera Onboarding for child Account Error: ' + str(response) + '.')
+        except Exception:
+            raise
+
+    # __get_account_cloudformation_template: Downloads the Archera Account-specific Onboarding template to local filesystem. Returns str.
+    def __get_account_cloudformation_template(self, httpHeaders: dict, child_account_id: str) -> str:
+        try:
+            r = self.http.request(
+                'GET', self.__get_base_url(account_id=child_account_id) + '/partners/onboarding/aws/cloudformation_template',
+                headers=httpHeaders
+            )
+            response = json.loads(r.data)
+            self.logger.debug('Get Account CloudFormation Template Response: ' + str(response))
+            if r.status != 200:
+                self.logger.exception(response)
+                if len(response.keys()) == 1:
+                    raise Exception('Unable to download CloudFormation template for child Account ' + str(list(response.keys())[0]).capitalize() + ': ' + str(response[list(response.keys())[0]]).capitalize() + '.')
+                elif 'code' in response.keys():
+                    raise Exception('Unable to download CloudFormation template for child Account Error ' + str(response['code']) + ': ' + response['status'] + '.')
+                else:
+                    raise Exception('Unable to download CloudFormation template for child Account Error: ' + str(response) + '.')
+        except Exception:
+            raise
+
+        return json.dumps(response['template'])
+        
+    def create_account(self, customer_account_name: str) -> tuple[bool, dict]:        
+        httpHeaders = self.__get_headers(api_key=self.partner_api_key)
+        self.logger.debug('HTTP Headers: ' + str(httpHeaders))
+
+        customer_account_id = None
+        cfnresponse_data = {}
+
+        self.logger.info('Starting Archera Onboarding')
+        customer_account_id = self.__create_child_account(httpHeaders=httpHeaders, partner_account_id=self.partner_account_id, child_account_name=customer_account_name)
+        cfnresponse_data.update({'ArcheraChildAccountId': customer_account_id})
+
+        self.logger.debug('Initiating onboarding for Customer Archera Account ID: ' + customer_account_id)
+        onboarding_id = self.__init_child_account_onboarding(httpHeaders=httpHeaders, child_account_id=customer_account_id)
+        cfnresponse_data.update({'ArcheraOnboardingId': customer_account_id})
+
+        if customer_account_id and onboarding_id:
+
+            self.logger.debug('Downloading the Archera Account-specific Onboarding template to runtime')
+            cfn_template_response = self.__get_account_cloudformation_template(httpHeaders=httpHeaders, child_account_id=customer_account_id)
+
+            if cfn_template_response:
+
+                # Setup S3
+                s3Copy = s3CopyFiles(
+                    logger=self.logger,
+                    region_name=self.region_name
+                )
+
+                cfn_template_pre_signed_url = s3Copy.s3_put_object(
+                    dst_bucket=environ.get('TEMPLATE_BUCKET_NAME'),  
+                    dst_key_prefix=environ.get('TEMPLATE_KEY_PREFIX'),      
+                    dst_key=customer_account_id,
+                    data=cfn_template_response
+                )
+
+                if cfn_template_pre_signed_url:                
+                    self.logger.info('Archera API Account creation for ' + customer_account_id + ' was successful')
+                    cfnresponse_data.update({'ArcheraAccountCreationStatus': 'Success'})
+                    cfnresponse_data.update({'ArcheraCloudFormationTemplateUrl': cfn_template_pre_signed_url})
+                    self.logger.info('Finished Archera Account Creation')
+                    return True, cfnresponse_data
+                else:
+                    self.logger.error('Archera API Account creation process for ' + customer_account_id + ' was not successful')
+                    cfnresponse_data.update({'ArcheraAccountCreationStatus': 'Needs Investigation'})
+                    return False, cfnresponse_data
+            else:
+                self.logger.error("Archera's API Account creation failed. Please refer to CloudWatch Logs for a detailed error message.")
+                return False, cfnresponse_data
---
+++ http-api-lambda-archera-api-onboarding/handler.py
@@ -0,0 +1,96 @@
+from os import environ
+import logging
+import cfnresponse
+import traceback
+import boto3
+
+# Setting up the logging level from the environment variable `LOGLEVEL`.
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(environ['LOGLEVEL'] if 'LOGLEVEL' in environ.keys() else 'INFO')
+
+# Setting up logging level specific to `botocore` from the environment variable `BOTOCORE_LOGLEVEL`.
+if 'BOTOCORE_LOGLEVEL' in environ.keys():
+    if environ['BOTOCORE_LOGLEVEL'] == 'DEBUG':
+        logger.info('Setting boto3 logging to DEBUG')
+        boto3.set_stream_logger('') # Log everything on boto3 messages to stdout
+    else:
+        logger.info('Setting boto3 logging to ' + environ['BOTOCORE_LOGLEVEL'])
+        boto3.set_stream_logger(level=logging._nameToLevel[environ['BOTOCORE_LOGLEVEL']]) # Log boto3 messages that match BOTOCORE_LOGLEVEL to stdout
+
+# Access environment variables
+base_url = environ.get('ARCHERA_BASE_URL')
+region_name = environ.get('REGION')
+customer_account_name_suffix = environ.get('CUSTOMER_ACCOUNT_NAME_SUFFIX')
+environment = environ.get('ENVIRONMENT')
+
+from archera.archera import Archera
+archera = Archera(
+    logger=logger,
+    base_url=base_url,
+    region_name=region_name
+)
+
+# lambda_handler: This script executes as a Custom Resource on the Onboarding CloudFormation stack, dynamically deploying CloudFormation stack from Ibexlabs partners. The script is executed when the stack is created, updated and removed.
+def lambda_handler(event, context):
+    
+    logger.debug('Environment variables - ' + str(environ))
+
+    # Parse HTTP Request Body for request parameters
+    customer_account_name = event['Request']
+    customer_aws_account_id = event['Requests']('CUSTOMER_AWS_ACCOUNT_ID')
+
+    # Create Stack - The following section gets executed when the deployed stack is created or updated using AWS CloudFormation.
+    if event['RequestType'] == 'Create':
+
+        try:
+            logger.debug('Create Stack Event - ' + str(event))
+
+            archera_onboarding_status, cfnresponse_data = archera.onboard(
+                customer_account_name=customer_account_name + 'c/o Ibexlabs'
+            ) # Archera needs `c/o Ibexlabs` suffix at the Partner portal level
+
+            if archera_onboarding_status:
+                cfnresponse.send(event, context, cfnresponse.SUCCESS, cfnresponse_data)
+            else:
+                raise Exception('Create Stack Error: Archera Onboarding failed')
+
+        # Handling `cfnresponse` error response when the stack creation failed and there is an exception in calling the API. 
+        except Exception as e:
+            logger.exception('Create Stack Error - ' + str(traceback.print_tb(e.__traceback__)))
+            cfnresponse.send(event, context, cfnresponse.FAILED, {})
+
+    # Update Stack - The following section gets executed when the deployed parent stack is updated using AWS CloudFormation. It tries to find the child stack and trigger an update.
+    if event['RequestType'] == 'Update':
+
+        try:
+            logger.debug('Update Stack Event - ' + str(event))
+
+            # TODO: Require an implementation to fetch a list of existing Archera Account IDs
+            # archera_update_stack_status = archera.update_stack(account_id=None)
+
+            # if archera_update_stack_status:
+            #     # TODO: Send cfnresponse data
+            #     cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+            # else:
+            #     raise Exception('Update Stack Error: Archera Update failed')
+        
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+
+        # Handling `cfnresponse` error response when the stack update failed and there is an exception in calling the API. 
+        except Exception as e:
+            logger.exception('Update Stack Error - ' + str(traceback.print_tb(e.__traceback__)))
+            # TODO: Send cfnresponse data
+            cfnresponse.send(event, context, cfnresponse.FAILED, {})
+
+    # Delete Stack - The following section gets executed when the deployed stack is deleted from AWS CloudFormation.
+    elif event['RequestType'] == 'Delete':
+
+        try:
+            logger.debug('Delete Stack Event - ' + str(event))
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+
+        # Handling `cfnresponse` error response when the stack is deleted but there is an exception in calling the API. 
+        except Exception as e:
+            logger.exception('Delete Stack Error - ' + str(traceback.print_tb(e.__traceback__)))
+            cfnresponse.send(event, context, cfnresponse.FAILED, {})
